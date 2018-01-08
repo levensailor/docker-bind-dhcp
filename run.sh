f10f571ac804ea62d531ea7b6189bbe69057755f
Spyros Vlachos <devspyrosv@users.noreply.github.com>
2018-01-08 02:30:30 +0200

    Expose host network interfaces
    Without the --net=host option, the container can only "see" its own interface and not the host's ones.

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@ docker run \
 -d \
 --restart=always \
 --dns=8.8.8.8 \
---publish=127.0.0.1:53:53/udp \
---publish=127.0.0.1:67:67/udp \
---publish=127.0.0.1:10000:10000 \
+--publish=192.168.0.1:53:53/udp \
+--publish=192.168.0.1:67:67/udp \
+--publish=192.168.0.1:10000:10000 \
 --volume=`pwd`/docker/bind-dhcp:/data \
 --env='ROOT_PASSWORD=CHANGETHEPASSWORD' \
+--net=host \
 devspyrosv/bind-dhcp-webmin:latest
